35b9bdc49b3e36e891625bcf900cefca853dae3d
Shubham <shubhamkumarverma091@gmail.com>
2025-10-31 07:31:19 +0530

    first commit

---
+++ frontend/src/landing_page/about/AboutPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../Navbar';
+import Hero from './Hero';
+import Team from './Team';
+import Footer from '../Footer';
+
+function AboutPAge() {
+    return ( 
+        <>
+       
+        <Hero />
+        <Team />
+        
+        </>
+     );
+}
+
+export default AboutPAge;
